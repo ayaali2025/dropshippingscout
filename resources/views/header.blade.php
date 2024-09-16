9ca57a54bf7c90d9ce8f91b1bfd78b9874dbba60
MahmoudHassan7 <mh600485@gmail.com>
2024-09-16 21:03:57 +0300

    edit on magicButtons

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -105,7 +105,7 @@
                             <li class="nav-item"><a class="nav-link" href="{{ route('pages.show', ['slug' => $pages->where('view_name', 'affiliate')->first()->slug]) }}">Affiliate</a></li>
                            <div class="magicButtons">
                             <button class="custom-btn btn-12"><span>For 1$!</span><span>Start now</span></button>
-                            <button class="custom-btn btn-10">login</button>
+                            <a href="https://app.dropshippingscout.com/login"><button class="custom-btn btn-10">login</button></a>
                            </div>
 
                         </ul>
