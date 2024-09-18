9ae4680b7513126784470690b83ee40176fe1a8b
MahmoudHassan7 <mh600485@gmail.com>
2024-09-18 16:20:14 +0300

    Update 404.blade.php

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -8,28 +8,6 @@
  
 @section('content')
 
-    <!-- Page Header Start -->
-	<div class="page-header">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<!-- Page Header Box Start -->
-					<div class="page-header-box">
-						<h1 class="text-anime-style-3">Page Not Found!</h1>
-						<nav class="wow fadeInUp" data-wow-delay="0.25s">
-							<ol class="breadcrumb">
-								<li class="breadcrumb-item"><a href="{{ url('/') }}">home</a></li>
-								<li class="breadcrumb-item active" aria-current="page">404 error</li>
-							</ol>
-						</nav>
-					</div>
-					<!-- Page Header Box End -->
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Page Header End -->
-
     <!-- error section start -->
     <div class="error-page">
         <div class="container">
