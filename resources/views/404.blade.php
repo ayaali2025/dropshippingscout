3302e25e8b08e4bc2af034ee29aa49a744b68f92
MahmoudHassan7 <mh600485@gmail.com>
2024-09-18 16:17:45 +0300

    Create 404.blade.php

---
+++ resources/views/404.blade.php
@@ -0,0 +1,51 @@
+ 
+@extends('layouts.master')
+
+@section('title', $page->title)
+@section('meta_description', $page->meta_description)
+@section('meta_keywords', $page->meta_keywords)
+@section('meta_author', $page->meta_author)
+ 
+@section('content')
+
+    <!-- Page Header Start -->
+	<div class="page-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- Page Header Box Start -->
+					<div class="page-header-box">
+						<h1 class="text-anime-style-3">Page Not Found!</h1>
+						<nav class="wow fadeInUp" data-wow-delay="0.25s">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="{{ url('/') }}">home</a></li>
+								<li class="breadcrumb-item active" aria-current="page">404 error</li>
+							</ol>
+						</nav>
+					</div>
+					<!-- Page Header Box End -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Page Header End -->
+
+    <!-- error section start -->
+    <div class="error-page">
+        <div class="container">
+            <div class="row">
+                <div class="error-page-image wow fadeInUp" data-wow-delay="0.25s">
+                    <img src="{{asset('images/404-error-img.png')}}" alt="">
+                </div>
+                <div class="error-page-content">
+                    <div class="error-page-content-heading">
+                        <h2 class="text-anime-style-3">Page Now Found!</h2>
+                        <p class="wow fadeInUp" data-wow-delay="0.5s">We're sorry, the page you requested could not be found.</p>
+                    </div>
+                    <a class="btn-default wow fadeInUp" data-wow-delay="0.75s" href="#">Back To Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- error section end -->
+@endsection
