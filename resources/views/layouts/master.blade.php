5c7146281d702997bb0f2737f1329d1816a986c7
MahmoudHassan7 <mh600485@gmail.com>
2024-09-18 15:57:45 +0300

    removing unused js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -103,8 +103,7 @@ function toggleVisibility(event, id) {
 <!-- SmoothScroll -->
 <script src="{{asset('js/SmoothScroll.js')}}"></script>
 <!-- MagicCursor js file -->
-<script src="{{asset('js/gsap.min.js')}}"></script>
-<script src="{{asset('js/magiccursor.js')}}"></script>
+
 <!-- Text Effect js file -->
 <script src="{{asset('js/SplitText.js')}}"></script>
 <script src="{{asset('js/ScrollTrigger.min.js')}}"></script>
